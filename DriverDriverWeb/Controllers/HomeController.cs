4135657de6853b5fb66b8f55c98b11ff2d13febe
j-ayala <86330524+j-ayala@users.noreply.github.com>
2021-06-30 23:43:44 -0500

    added database to the project and the view called My Tracker to track your golf rounds

---
+++ DriverDriverWeb/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using DataLayer;
+using DriverDriverWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DriverDriverWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyTracker()
+        {
+            List<CourseModel> courseList = new List<Models.CourseModel>();
+            using (GolfRecordsDBEntities db = new GolfRecordsDBEntities())
+            {
+                var courses = db.tblCourses.ToList();
+                foreach (var course in courses)
+                {
+                    CourseModel model = new CourseModel();
+
+                    model.ID = course.CourseID;
+                    model.CourseName = course.CourseName;
+                    model.Location = course.Location;
+                    model.Price = course.Price;
+                    model.TimeSpent = course.TimeSpent;
+                    model.BallsLost = course.BallsLost;
+                    model.Score = course.Score;
+
+                    courseList.Add(model);
+                }
+            }
+            return View(courseList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DriverDriverWeb/Models/CourseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DriverDriverWeb.Models
+{
+    public class CourseModel
+    {
+        public int ID { get; set; }
+        public string CourseName { get; set; }
+        public string Location { get; set; }
+        public decimal? Price { get; set; }
+        public string TimeSpent { get; set; }
+        public int? BallsLost { get; set; }
+        public int? Score { get; set; }
+    }
+}
